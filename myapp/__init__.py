e6364dfaad3b8082427280c7c6f540650878bcb0
jiminxchris <jiminxchris@gmail.com>
2025-01-17 19:29:22 +0900

    waitress로 수정

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from waitress import serve
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -16,53 +17,54 @@ db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 # db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app():
-    app = Flask(__name__)
-    # app.config.from_object(config)
-    app.config.from_envvar('APP_CONFIG_FILE')
+#def create_app():
+app = Flask(__name__)
+# app.config.from_object(config)
+app.config.from_envvar('APP_CONFIG_FILE')
 
-    # ORM
-    db.init_app(app)
-    # migrate.init_app(app, db)
-    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
-        migrate.init_app(app, db, render_as_batch=True)
-    else:
-        migrate.init_app(app, db)
-    from . import models
+# ORM
+db.init_app(app)
+# migrate.init_app(app, db)
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+    migrate.init_app(app, db, render_as_batch=True)
+else:
+    migrate.init_app(app, db)
+from . import models
 
-    # @app.route('/')
-    # def hello_myapp():
-    #     return 'Hello, First my App!'
+# @app.route('/')
+# def hello_myapp():
+#     return 'Hello, First my App!'
 
-    # Blueprint
-    # from .views import main_views
-    # app.register_blueprint(main_views.bp)
+# Blueprint
+# from .views import main_views
+# app.register_blueprint(main_views.bp)
 
-    from .views import main_views, question_views, answer_views, auth_views
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
+from .views import main_views, question_views, answer_views, auth_views
+app.register_blueprint(main_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
 
-    #블루프린트가 없을경우
-    # 라우트 정의
-    # @app.route('/')
-    # def index():
-    #     return 'Hello, First my App!'
+#블루프린트가 없을경우
+# 라우트 정의
+# @app.route('/')
+# def index():
+#     return 'Hello, First my App!'
 
-    # @app.route('/questions')
-    # def questions():
-    #     return 'Questions Page'
+# @app.route('/questions')
+# def questions():
+#     return 'Questions Page'
 
-    # @app.route('/answers')
-    # def answers():
-    #     return 'Answers Page'
+# @app.route('/answers')
+# def answers():
+#     return 'Answers Page'
 
-    # 라우팅
-    # @app.route('/')
+# 라우팅
+# @app.route('/')
 
-    # 필터
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+# 필터
+from .filter import format_datetime
+app.jinja_env.filters['datetime'] = format_datetime
     
-    return app
+    #return app
+serve(app, listen="*:80")
