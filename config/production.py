596c49943a46ce52003663756f5f3a21ed56af56
jiminxchris <jiminxchris@gmail.com>
2025-01-17 23:35:38 +0900

    다시 설정

--- config/production.py
+++ config/production.py
@@ -1,12 +1,13 @@
 from config.default import *
 from dotenv import load_dotenv
+from urllib.parse import quote
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'myapp.db'))
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
-    pw=os.getenv('DB_PASSWORD'),
+    pw=quote(os.getenv('DB_PASSWORD')),
     url=os.getenv('DB_HOST'),
     db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
