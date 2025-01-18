18b8101a3d044a92c1e2a7dc6c9af7d2611c1ff8
jiminxchris <jiminxchris@gmail.com>
2025-01-18 20:21:01 +0900

    파일정리

--- myapp/test.py
+++
@@ -1,8 +0,0 @@
-from myapp import db
-from myapp.models import User
-
-anonymous_user  = User(username='Anonymous', 
-                        password = '9999',
-                        email='anonymous@example.com')
-db.session.add(anonymous_user)
-db.session.commit()
